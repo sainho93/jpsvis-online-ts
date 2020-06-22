fe50ec4f3338b633bf8878cb2c401c1613ef9baf
Tao Zhong <taozhongimd@gmail.com>
2020-06-22 17:27:17 +0200

    feat: Upload xml files to server
    Using aiohttp_cors for POST handler

--- src/components/upload-page.js
+++ src/components/upload-page.js
@@ -73,86 +73,87 @@ class UploadPage extends React.Component {
     }
   }
 
-    onChangeHandler=event => {
-      this.setState({
-        selectedFile: event.target.files[0],
-        loaded: 0
+  /* Event handlers */
+  onChangeHandler=event => {
+    this.setState({
+      selectedFile: event.target.files[0],
+      loaded: 0
+    })
+  }
+
+  // Dragger uses rc-upload component
+  // custom upload method to upload files
+  uploadToLocal = async options => {
+    const data = new FormData()
+    const { onSuccess, onError, file } = options
+
+    data.append('file', this.state.selectedFile)
+
+    axios
+      .post('http://localhost:8080/upload/', data, {})
+      .then(res => {
+        onSuccess(file)
+        console.log(res)
       })
-    }
+      .catch(err => {
+        const error = new Error('Some error')
+        onError({ event: error })
+      })
+  }
 
-    // Dragger uses rc-upload component
-    // custom upload method to upload the file to local rather sever
-    uploadToLocal = async options => {
-      const data = new FormData()
-      const { onSuccess, onError, file } = options
+  next () {
+    // move to next step
+    const current = this.state.current + 1
+    this.setState({ current })
+  }
 
-      data.append('file', this.state.selectedFile)
+  prev () {
+    const current = this.state.current - 1
+    this.setState({ current })
+  }
 
-      axios
-        .post('http://localhost:8000/upload', data, {})
-        .then(res => {
-          onSuccess(file)
-          console.log(res)
-        })
-        .catch(err => {
-          const error = new Error('Some error')
-          onError({ event: error })
-        })
-    }
-
-    next () {
-      // move to next step
-      const current = this.state.current + 1
-      this.setState({ current })
-    }
-
-    prev () {
-      const current = this.state.current - 1
-      this.setState({ current })
-    }
-
-    render () {
-      const { current } = this.state
-      return (
-        <div>
-          <Steps current={current}>
-            {steps.map(item => (
-              <Step key={item.title} title={item.title} />
-            ))}
-          </Steps>
-          <div className="steps-content" onChange={this.onChangeHandler}>
-            <Dragger {...dragger} accept='.xml' customRequest={this.uploadToLocal}>
-              <p className="ant-upload-drag-icon">
-                <InboxOutlined />
-              </p>
-              <p className="ant-upload-text">
-                            Click or drag file to this area to upload
-              </p>
-              <p className="ant-upload-hint">
-                            Support only for xml files of JuPedSim
-              </p>
-            </Dragger>
-          </div>
-          <div className="steps-action">
-            {current < steps.length - 1 && (
-              <Button type="primary" onClick={() => this.next()}>
-                            Next
-              </Button>
-            )}
-            {current === steps.length - 1 && (
-              <Button type="primary" onClick={() => message.success('Processing complete!')}>
-                <Link to="/ViewPage">Start Visualization</Link>
-              </Button>
-            )}
-            {current > 0 && (
-              <Button style={{ margin: '0 8px' }} onClick={() => this.prev()}>
-                            Previous
-              </Button>
-            )}
-          </div>
+  render () {
+    const { current } = this.state
+    return (
+      <div>
+        <Steps current={current}>
+          {steps.map(item => (
+            <Step key={item.title} title={item.title} />
+          ))}
+        </Steps>
+        <div className="steps-content" onChange={this.onChangeHandler}>
+          <Dragger {...dragger} accept='.xml' customRequest={this.uploadToLocal}>
+            <p className="ant-upload-drag-icon">
+              <InboxOutlined />
+            </p>
+            <p className="ant-upload-text">
+                          Click or drag file to this area to upload
+            </p>
+            <p className="ant-upload-hint">
+                          Support only for xml files of JuPedSim
+            </p>
+          </Dragger>
         </div>
-      )
-    }
+        <div className="steps-action">
+          {current < steps.length - 1 && (
+            <Button type="primary" onClick={() => this.next()}>
+                          Next
+            </Button>
+          )}
+          {current === steps.length - 1 && (
+            <Button type="primary" onClick={() => message.success('Processing complete!')}>
+              <Link to="/ViewPage">Start Visualization</Link>
+            </Button>
+          )}
+          {current > 0 && (
+            <Button style={{ margin: '0 8px' }} onClick={() => this.prev()}>
+                          Previous
+            </Button>
+          )}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default UploadPage
--- src/server/server.py
+++ src/server/server.py
@@ -1,5 +1,7 @@
 from aiohttp import web
+import aiohttp_cors
 import os
+import json
 import asyncio
 import logging
 import pathlib
@@ -17,28 +19,72 @@ async def index(request):
     return web.Response(text=html, content_type='text/html', headers=NO_CACHE_HEADER)
 
 
+# Upload request handler
+async def post_file(request):
+    try:
+        reader = await request.multipart()
+        file = await reader.next()
+        filename = file.filename if file.filename else 'undefined'
+
+        print('here: ', filename, os.getcwd())
+
+        size = 0
+        with open(filename, 'wb') as f:
+            while True:
+                chunk = await file.read_chunk()
+                if not chunk:
+                    break
+                size += len(chunk)
+                f.write(chunk)
+
+        text = {'res': '200'}
+        return web.Response(text=json.dumps(text, ensure_ascii=False)) # Response to Dragger component
+    except Exception as e:
+        print(e)
+        return web.Response(text="500") # Response to Dragger component
+
+
+
 # Set-up web app
 async def init():
     app = web.Application()
+
     aiohttp_debugtoolbar.setup(app)
 
+    # CORS Implementation
+    # `aiohttp_cors.setup` returns `aiohttp_cors.CorsConfig` instance.
+    # The `cors` instance will store CORS configuration for the application.
+    cors = aiohttp_cors.setup(app, defaults={
+        "*": aiohttp_cors.ResourceOptions(
+            allow_credentials=True,
+            expose_headers="*",
+            allow_headers="*",
+            # allow_methods="*",
+        )
+    })
+    resource = cors.add(app.router.add_resource("/upload/"))
+    cors.add(resource.add_route("POST", post_file)) # Add handler for POST request on ./upload
+
+    # Routers
     app.router.add_get("/", index)
     app.router.add_static('/', path=str(PROJ_ROOT / 'static'))
+    #TODO: Fix CSS file error
+
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, '0.0.0.0', 8080)
+
+    print('------- Http server started at localhost:8080 --------')
+
+    await site.start()
 
     return app
 
 
-# Set-up event loop
-def main():
+if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
 
     loop = asyncio.get_event_loop()
-    app = loop.run_until_complete(init())
-
-    web.run_app(app)
-
+    loop.run_until_complete(init())
     loop.run_forever()
-
-
-if __name__ == '__main__':
-    main()
+    #TODO: Fix run_forever error
