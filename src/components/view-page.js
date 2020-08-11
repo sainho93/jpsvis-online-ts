6d9d9e11b3c170a0d1f354fac7b52a4c99ba5ff9
Tao Zhong <taozhongimd@gmail.com>
2020-08-11 16:53:02 +0200

    feat: Show N_t plot in new window

--- src/components/view-page.js
+++ src/components/view-page.js
@@ -25,14 +25,23 @@ import React from 'react'
 import './view-page.css'
 import init from '../initialization';
 import JPS3D from '../3Dvisualization/JPS3D'
+import NewWindow from 'react-new-window'
 
 import { Button, Cascader, Layout, Space } from 'antd'
 import Dragger from 'antd/es/upload/Dragger'
 import Sider from 'antd/es/layout/Sider'
-const { Content, Footer} = Layout;
+const { Content } = Layout;
 import { InboxOutlined } from '@ant-design/icons'
+import axios from 'axios'
 
 class ViewPage extends React.Component {
+  state = {
+    opened: false,
+    url: 'N_t',
+    selectedFile: null,
+    imgData: ''
+  }
+
   // Load Three.JS after <canvas/> node is mounted into DOM
   componentDidMount(){
     (async () => {
@@ -51,21 +60,142 @@ class ViewPage extends React.Component {
     }
   }
 
+  toggleOpened() {
+    fetch('N_t')
+      .then(response => response.text())
+      .then(data => {
+        this.setState(prevState => ({imgData: data}))
+        const img = "data:image/png;base64," + data;
+        console.log(img)
+      });
+    this.setState(prevState => ({opened: !prevState.opened}))
+  }
+
+  newWindowUnloaded() {
+    this.setState({ opened: false })
+  }
+
+  /* Event handlers */
+  onChangeHandler=event => {
+    this.setState({
+      selectedFile: event.target.files[0],
+      loaded: 0
+    })
+  }
+
+  // Dragger uses rc-upload component
+  // custom upload method to upload files
+  uploadToLocal = async options => {
+    const data = new FormData()
+    const { onSuccess, onError, file } = options
+
+    data.append('file', this.state.selectedFile)
+
+    axios
+      .post('http://localhost:8080/upload', data, {})
+      .then(res => {
+        onSuccess(file)
+        console.log(res)
+      })
+      .catch(err => {
+        const error = new Error('Some error')
+        onError({ event: error })
+      })
+  }
+
   render () {
+    const options = [
+      {
+        value: 'gait',
+        label: 'Gait'
+      },
+      {
+        value: 'N-t',
+        label: 'N-t Diagram'
+      },
+      {
+        value: 'flow_velocity',
+        label: 'Flow - Velocity'
+      },
+      {
+        value: 'density_velocity',
+        label: 'Density - Velocity',
+        children: [
+          {
+            value: 'b',
+            label: 'Method B'
+          },
+          {
+            value: 'c',
+            label: 'Method C'
+          },
+          {
+            value: 'd',
+            label: 'Method D'
+          }
+        ]
+      },
+      {
+        value: 'density',
+        label: 'Voronoi Diagram',
+        children: [
+          {
+            value: 'density_voronoi',
+            label: 'Density',
+          },
+          {
+            value: 'velocity_voronoi',
+            label: 'Velocity'
+          }
+        ]
+      },
+      {
+        value: 'profiles',
+        label: 'Profiles',
+        children: [
+          {
+            value: 'p_density',
+            label: 'Density'
+          },
+          {
+            value: 'p_velocity',
+            label: 'Velocity'
+          },
+          {
+            value: 'p_specific',
+            label: 'Specific Flow'
+          }
+        ]
+      }
+    ]
+
+    const {opened} = this.state
+
     return (
       <Layout className="view-page-layout">
         <Sider width={"25%"} theme={"light"} className="view-page-sider">
           <Space direction="vertical" align="center">
-            <Dragger>
-              <p className="ant-upload-drag-icon">
-                <InboxOutlined />
-              </p>
-              <p className="ant-upload-text">Click or drag output files from JPSReport to this area to upload</p>
-            </Dragger>
+            <div onChange={this.onChangeHandler}>
+              <Dragger accept='.xml, .dat, .txt' customRequest={this.uploadToLocal}>
+                <p className="ant-upload-drag-icon">
+                  <InboxOutlined />
+                </p>
+                <p className="ant-upload-text">Click or drag output files from JPSReport to this area to upload</p>
+              </Dragger>
+            </div>
             <>
               <Space>
-                <Cascader options={this.options}/>
-                <Button>Start Plot</Button>
+                <Cascader options={options}/>
+                <Button onClick={() => this.toggleOpened()}>
+                  Start Plot
+                </Button>
+                {opened && (
+                  <NewWindow
+                    onUnload={() => this.newWindowUnloaded()}
+                  >
+                    <img src={"data:image/png;base64," + this.state.imgData}/>
+                  </NewWindow>
+                )}
               </Space>
             </>
           </Space>
